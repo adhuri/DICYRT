754cb7a73621e18913654ce164849134cfa9da28
aniketdhuri <aniket.dhuri@gmail.com>
2016-10-20 21:24:57 -0400

    MongoDb support

--- code/ui/server.js
+++ code/ui/server.js
@@ -2,8 +2,79 @@ var http = require('http');
 var fs = require('fs');
 var formidable = require("formidable");
 var util = require('util');
-const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({ contactPoints: ['152.46.19.234'], keyspace: 'kspace'});
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://152.46.19.234:27017/test';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+     var collection = db.collection('business');
+
+    // do some work here with the database.
+
+    collection.find({name:"Rocky's Lounge"}).toArray(function (err, result) {
+     if (err) {
+       console.log(err);
+     } else if (result.length) {
+       console.log('Found:', result);
+     } else {
+       console.log('No document(s) found with defined "find" criteria!');
+     }
+   });
+   //Group By query
+   /*var query ={
+	"key" : {
+		"food" : true,
+		"restaurant_name" : true
+	},
+	"initial" : {
+		"countfood" : 0
+	},
+	"reduce" : function (obj, prev) {         if (obj.food != null) if (obj.food instanceof Array) prev.countfood += obj.food.length;         else prev.countfood++;     },
+	"cond" : {
+		"location_city" : "Dravosburg",
+		"food" : "Nachos"
+	}
+}*/
+
+var keys ={
+ "food" : true,
+ "restaurant_name" : true
+}
+var condition={
+ "location_city" : "Dravosburg",
+ "food" : "Nachos"
+}
+var initial={
+ "countfood" : 0
+}
+
+   collection.group(keys,condition,initial,function (obj, prev) {         if (obj.food != null) if (obj.food instanceof Array) prev.countfood += obj.food.length;         else prev.countfood++;     }),(function (err, result) {
+    if (err) {
+      console.log(err);
+    } else if (result.length) {
+      console.log('Found:', result);
+    } else {
+      console.log('No document(s) found with defined "find" criteria!');
+    }
+  });
+
+    //Close connection
+    db.close();
+  }
+});
+
+
 const query = 'SELECT * from business';
 
 var server = http.createServer(function (req, res) {
@@ -46,7 +117,7 @@ function processAllFieldsOfTheForm(req, res) {
             'content-type': 'text/plain'
         });
         res.write('received the data:\n\n');
-        
+
         res.end(util.inspect({
             fields: fields,
             files: files
