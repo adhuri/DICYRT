9d84942c10046feef4e7110577bb4df8b431a0db
amrutas16 <amruta.s16@gmail.com>
2016-11-03 11:41:32 -0400

    Add analyse.py

---
+++ analysis/analyse.py
@@ -0,0 +1,58 @@
+from pyspark import SparkConf, SparkContext
+import re
+import json
+import config
+
+sc = None
+words = None
+
+
+def extract_food_items(review):
+    food_items = []
+    for word in words:
+        if word in review['text']:
+            business_id = review['business_id']
+            food_items.append(business_id + " " + word)
+    return food_items
+
+
+def load_wordlist(filename):
+    text = sc.textFile(filename,4)
+    words = text.flatMap(lambda word: word.split("\n"))
+    return words.collect()
+
+
+def load_reviews(filename):
+    text = sc.textFile(filename,4)
+    return text
+
+
+def parse_json(review):
+    review = json.loads(review)
+    return {'business_id': review['business_id'], 'text': review['text']}
+
+
+# returns a tuple (business id, food item, count)
+def create_three_tuples(data):
+    arr = data[0].split(" ");
+    return (arr[0], arr[1], data[1])
+
+
+def main():
+    global sc, words
+    conf = SparkConf().setMaster(config.spark['server']).setAppName(config.spark['appname']).set("spark.driver.maxResultSize", "0").set("spark.executor.heartbeatInterval","600")
+    #conf = SparkConf().setMaster('local[2]').setAppName(config.spark['appname']).set("spark.driver.maxResultSize", "0").set("spark.executor.heartbeatInterval","600")
+    sc = SparkContext(conf=conf)
+    words = load_wordlist(config.foodlist)
+    reviews = load_reviews(config.reviewlist)
+    reviews = reviews.map(parse_json)
+    reviews.cache()
+    businessid_food_count = reviews.flatMap(extract_food_items).distinct().map(lambda word: (word,1)).reduceByKey(lambda a,b : a + b).map(create_three_tuples)
+    #print businessid_food_count.collect()[0]
+    
+
+def filterSpecChars(inp):
+        #Following approach is inspired from a StackOverflow post
+	return re.sub('[^A-Za-z0-9\s]', '', inp).lower()
+
+main()
