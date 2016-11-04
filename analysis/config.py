c2c31f06d6ef8152a8e0008b759f976544237e67
amrutas16 <amruta.s16@gmail.com>
2016-11-04 14:32:13 -0400

    Update config.py

--- analysis/config.py
+++ analysis/config.py
@@ -3,6 +3,6 @@ spark = dict(
     appname = 'YelpAnalyser'
     )
 
-foodlist='foodDict.txt'
+foodlist='/home/adhuri/DICYRT/analysis/foodDict.txt'
 reviewlist='/home/adhuri/DICYRT/data/yelp_academic_dataset_review_part.json'
 #reviewlist='/home/adhuri/DICYRT/data/yelp_academic_dataset_review.json'
