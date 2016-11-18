1265811deb10ecb98a6e1e6344f10c6b1295865c
adhuri <aniket.dhuri@gmail.com>
2016-11-18 06:15:19 -0500

    Added run_analyse_streaming.sh to pull from kafka

---
+++ analysis/analyse_streaming.py
@@ -0,0 +1,22 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+sc = None
+
+def main():
+    sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
+    ssc = StreamingContext(sc, 10)   # Create a streaming context with batch interval of 10 sec
+    
+    kafkaStream = KafkaUtils.createDirectStream(ssc,['google_places'],kafkaParams = {"metadata.broker.list": '152.46.16.173:9092', 'auto.offset.reset': 'smallest'})
+    gplaces = kafkaStream.foreachRDD(lambda x: pprint(x))
+   	
+    ssc.start()             
+    ssc.awaitTermination()  	
+
+def pprint(x):
+	print x.collect()
+
+if __name__=="__main__":
+    main()
+
