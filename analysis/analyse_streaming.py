816ed4a5b6c48bd5ea23f143d43bac9bf152aaeb
adhuri <aniket.dhuri@gmail.com>
2016-11-22 02:43:14 -0500

    Making work for slave nodes

--- analysis/analyse_streaming.py
+++ analysis/analyse_streaming.py
@@ -1,22 +1,80 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+import re
 import json
+import config
+#import cass 
 sc = None
+words = None
 
 def main():
-    sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
-    ssc = StreamingContext(sc, 10)   # Create a streaming context with batch interval of 10 sec
-    
-    kafkaStream = KafkaUtils.createDirectStream(ssc,['google_places'],kafkaParams = {"metadata.broker.list": '152.46.16.173:9092', 'auto.offset.reset': 'smallest'})
-    gplaces = kafkaStream.foreachRDD(lambda x: pprint(x))
-   	
-    ssc.start()             
-    ssc.awaitTermination()  	
+	global sc, words
+	conf = SparkConf().setMaster('local[2]').setAppName(config.spark['appname']).set("spark.driver.maxResultSize", "0").set("spark.executor.heartbeatInterval","600")
+	sc = SparkContext(conf=conf)
+	ssc = StreamingContext(sc, 10)   # Create a streaming context with batch interval of 10 sec
+	words = load_wordlist(config.foodlist)
+	reviews = stream(ssc, 10)
+	for r in reviews:
+		if len(r) > 0:
+			rd = sc.parallelize(r)
+			process(rd)
 
-def pprint(x):
-	print x.collect()
+def process(rd):
+	rd = rd.map(parse_json)
+	rd.cache()
+	businessid_food_count_list = rd.flatMap(extract_food_items).map(lambda bid_fooditem: (bid_fooditem,1)).reduceByKey(lambda a,b : a + b)\
+								 .map(create_tuple).collect()
+	print businessid_food_count_list
+
+def stream(ssc, duration):
+	reviews = []
+	kafkaStream = KafkaUtils.createDirectStream(ssc,['google_places'],kafkaParams = {"metadata.broker.list": '152.46.16.173:9092', 'auto.offset.reset': 'smallest'})
+	objstream = kafkaStream.map(lambda x: x[1])
+	objstream.foreachRDD(lambda rdd: reviews.append(rdd.collect()))
+
+	ssc.start()
+	#ssc.awaitTermination()
+	ssc.awaitTerminationOrTimeout(duration)
+	ssc.stop(stopGraceFully=True) 	
+	return reviews
+
+def extract_food_items(review):
+	food_items = []
+	for word in words:
+		if word in review['text']:
+			business_id = review['business_id']
+			food_items.append(business_id + " " + word)
+	return food_items
+
+def load_wordlist(filename):
+	text = sc.textFile(filename,4)
+	#words = text.flatMap(lambda word: word.split("\n"))
+	words = text.flatMap(lambda word: word.split("\n")).map(lambda word: word.split(":")[0])
+	return words.collect()
+
+
+def load_reviews(filename):
+	text = sc.textFile(filename,4)
+	return text
+
+
+def parse_json(review):
+	review = json.loads(review)
+	return {'business_id': review['business_id'], 'text': review['text']}
+
+
+# returns a tuple (business id, food item, count)
+def create_tuple(data):
+	arr = data[0].split(" ");
+	element = {'business_id': arr[0], 'food': arr[1], 'count': data[1]}
+	#cass.insert_food_details(element,"Yelp")
+	return element
+
+def filterSpecChars(inp):
+		#Following approach is inspired from a StackOverflow post
+	return re.sub('[^A-Za-z0-9\s]', '', inp).lower()
 
 if __name__=="__main__":
-    main()
+	main()
 
