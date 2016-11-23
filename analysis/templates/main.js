7a63471bf8d241990cc851d5648e1d77ae25c2bd
ashintr <ashintr@ncsu.edu>
2016-11-23 14:40:50 -0500

    Updates

--- analysis/templates/main.js
+++ analysis/templates/main.js
@@ -3,7 +3,6 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"));
 
     $.get('restaurantDetails.json', function(data) {
-        // console.log(data.restaurants)
         var markers = data.restaurants
         for( i = 0; i < markers.length; i++ ) 
         {
@@ -13,25 +12,10 @@ function initMap() {
             marker = new google.maps.Marker({
                 position: position,
                 map: map,   
-                title: marker.name
+                title: marker.restName
             });
             map.fitBounds(bounds);
         }
     }, 'json');
 
-    // var markers = [
-    //     ['Rocky\'s lounge', 40.3964688, -80.0849416]
-    // ];
-
-    // for( i = 0; i < markers.length; i++ ) {
-    //     var marker = markers[i];
-    //     var position = new google.maps.LatLng(marker[1], marker[2]);
-    //     bounds.extend(position);
-    //     marker = new google.maps.Marker({
-    //         position: position,
-    //         map: map,
-    //         title: marker[0]
-    //     });
-    //     map.fitBounds(bounds);
-    // }
 }
