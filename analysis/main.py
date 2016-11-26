ef8c2c9908599c388ed607e840afc4e7dbdeac9f
amrutas16 <amruta.s16@gmail.com>
2016-11-25 22:22:49 -0500

    Update main.py

--- analysis/main.py
+++ analysis/main.py
@@ -33,7 +33,6 @@ def search_query_2(restaurant,location):
         return result	
         cass.setLog("INFO", "Making API call for google_places_kafka")
 
-def 
 
 def get_top10_restaurant():
     foodList= getFoodlist()
